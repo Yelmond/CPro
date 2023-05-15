37a0f6a71d6e275266dabff6e98a2b0ad9c39882
damian <damian.campeanu@gmail.com>
2023-05-14 23:06:31 -0400

    wip inital setup

---
+++ run.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+export CPRO_LOCAL=".local"
+export CPRO_WORK=".work"
+
+export CPRO_CMAKE_VERSION="3.26.3"
+export CPRO_NINJA_VERSION="1.11.1"
+
+export CPRO_CMAKE="${CPRO_LOCAL}/cmake/bin/cmake.exe"
+export CPRO_NINJA="${CPRO_LOCAL}/ninja/ninja.exe"
+export CPRO_VCPKG="${CPRO_LOCAL}/vcpkg/vcpkg.exe"
+
+#export PATH="${CPRO_LOCAL}/cmake/bin:${CPRO_LOCAL}/ninja:${CPRO_LOCAL}/vcpkg:${PATH}"
+
+if [ "$1" == "setup" ]; then
+
+	mkdir -p ${CPRO_LOCAL}
+	cd ${CPRO_LOCAL}
+
+	curl -L -O https://github.com/Kitware/CMake/releases/download/v${CPRO_CMAKE_VERSION}/cmake-${CPRO_CMAKE_VERSION}-windows-x86_64.zip || exit $?
+	unzip -q cmake-${CPRO_CMAKE_VERSION}-windows-x86_64.zip || exit $?
+	mv cmake-${CPRO_CMAKE_VERSION}-windows-x86_64 cmake || exit $?
+
+	curl -L -O https://github.com/ninja-build/ninja/releases/download/v${CPRO_NINJA_VERSION}/ninja-win.zip || exit $?
+	unzip -q ninja-win.zip -d ninja || exit $?
+
+	git clone --depth=1 https://github.com/Microsoft/vcpkg.git || exit $?
+	cd vcpkg
+	./bootstrap-vcpkg.bat -disableMetrics || exit $?
+
+	cd ../..
+
+elif [ "$1" == "project" ]; then
+
+	${CPRO_CMAKE} --preset $2 || exit $?
+
+elif [ "$1" == "build" ]; then
+
+	${CPRO_CMAKE} --build --preset $2 || exit $?
+
+elif [ "$1" == "run" ]; then
+
+	ls || exit $?
+
+elif [ "$1" == "reset-local" ]; then
+
+	rm -rf ${CPRO_LOCAL} || exit $?
+
+elif [ "$1" == "reset-work" ]; then
+
+	rm -rf ${CPRO_WORK} || exit $?
+
+fi
