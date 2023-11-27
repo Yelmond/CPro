116e00f514b58203a3105f4f3b872b2b2fc6fb0c
damian <damian.campeanu@gmail.com>
2023-11-27 03:09:19 -0500

    fixed shell help

--- shell/help.sh
+++ shell/help.sh
@@ -4,7 +4,7 @@ function func_help() {
 	echo "  open [sln|vs]     Open the project in Visual Studio. 'sln' opens the .sln file, 'vs' opens Visual Studio."
 	echo "  project [preset]  Use a preset for the project."
 	echo "  build [preset]    Build the project using a preset."
-	echo "  run [project]     Run the specified project."
+	echo "  run [preset]      Run the specified preset."
 	echo "  resetlocal        Delete all files in the local directory."
 	echo "  resetwork         Delete all files in the work directory."
 }
