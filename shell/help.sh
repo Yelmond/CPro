5dd31035cc71071cd3300e49f9cf869eaeb92ca5
damian <damian@unity3d.com>
2024-01-14 16:36:16 -0500

    updated run.sh help

--- shell/help.sh
+++ shell/help.sh
@@ -1,10 +1,23 @@
 function func_help() {
 	echo "Available commands:"
+	echo ""
 	echo "  setup             Set up the environment."
-	echo "  open [sln|vs]     Open the project in Visual Studio. 'sln' opens the .sln file, 'vs' opens Visual Studio."
+	echo "  open [sln|vs]     Open the project in Visual Studio."
+	echo "                    'sln' opens the .sln file,"
+	echo "                    'vs' opens Visual Studio."
 	echo "  project [preset]  Use a preset for the project."
 	echo "  build [preset]    Build the project using a preset."
 	echo "  run [preset]      Run the specified preset."
+	echo "  br [preset]       Build and run the specified preset."
 	echo "  resetlocal        Delete all files in the local directory."
 	echo "  resetwork         Delete all files in the work directory."
+	echo ""
+
+	if [ ! -f "${CPRO_CMAKE}" ]; then
+		echo "Available presets:"
+		echo "  CMake not found. Run 'setup' to set up the environment."
+		return 0
+	fi
+
+	${CPRO_CMAKE} --list-presets=all || return $?
 }
