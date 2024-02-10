114690aaaec484535c6fceb5ac4e7ae5f72f3f75
damian <damian@unity3d.com>
2024-02-10 17:18:57 -0500

    fixed non-Windows run.sh

--- run.sh
+++ run.sh
@@ -6,10 +6,10 @@ export CPRO_WORK=".work"
 export CPRO_CMAKE_VERSION="3.27.9"
 export CPRO_NINJA_VERSION="1.11.1"
 
-export CPRO_CMAKE="${CPRO_LOCAL}/cmake/bin/cmake.exe"
-export CPRO_CTEST="${CPRO_LOCAL}/cmake/bin/ctest.exe"
-export CPRO_NINJA="${CPRO_LOCAL}/ninja/ninja.exe"
-export CPRO_VCPKG="${CPRO_LOCAL}/vcpkg/vcpkg.exe"
+export CPRO_CMAKE="${CPRO_LOCAL}/cmake/bin/cmake"
+export CPRO_CTEST="${CPRO_LOCAL}/cmake/bin/ctest"
+export CPRO_NINJA="${CPRO_LOCAL}/ninja/ninja"
+export CPRO_VCPKG="${CPRO_LOCAL}/vcpkg/vcpkg"
 
 if [ "$1" == "" -o "$1" == "help" -o "$1" == "-?" ]; then
 	source shell/help.sh
