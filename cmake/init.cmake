ebec6eaf2bef802508940ffa72ac432b71c07325
damian <damian@unity3d.com>
2024-02-10 18:11:50 -0500

    minor cleanup

--- cmake/init.cmake
+++ cmake/init.cmake
@@ -6,8 +6,4 @@ macro ( init_cxx )
 	set( CMAKE_CXX_STANDARD_REQUIRED ON )
 	set( CMAKE_CXX_EXTENSIONS OFF )
 
-	# if ( CMAKE_CXX_COMPILER_ID STREQUAL "Clang" AND NOT WIN32 )
-	# 	set( CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -stdlib=libc++" )
-	# endif ()
-
 endmacro ()
