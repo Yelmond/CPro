0d4c6db0ab0fe83c2731be74c4633a9c183793ab
damian <damian@unity3d.com>
2024-02-10 17:49:04 -0500

    set CXX standared as required

--- cmake/init.cmake
+++ cmake/init.cmake
@@ -3,9 +3,11 @@ include_guard( GLOBAL )
 macro ( init_cxx )
 
 	set( CMAKE_CXX_STANDARD 23 )
+	set( CMAKE_CXX_STANDARD_REQUIRED ON )
+	set( CMAKE_CXX_EXTENSIONS OFF )
 
-	if ( CMAKE_CXX_COMPILER_ID STREQUAL "Clang" AND NOT WIN32 )
-		set( CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -stdlib=libc++" )
-	endif ()
+	# if ( CMAKE_CXX_COMPILER_ID STREQUAL "Clang" AND NOT WIN32 )
+	# 	set( CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -stdlib=libc++" )
+	# endif ()
 
 endmacro ()
